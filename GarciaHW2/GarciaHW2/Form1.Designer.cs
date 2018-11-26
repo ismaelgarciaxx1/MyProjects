1acd4d1a8d04245862c8da1bcf2a3d8ccebf6a1a
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2018-11-26 12:02:31 -0700

    Add files via upload

---
+++ GarciaHW2/GarciaHW2/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace GarciaHW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.southTextBox = new System.Windows.Forms.TextBox();
+            this.eastTextBox = new System.Windows.Forms.TextBox();
+            this.westTextBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.southLabel = new System.Windows.Forms.Label();
+            this.eastLabel = new System.Windows.Forms.Label();
+            this.westLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.southTextBox);
+            this.groupBox1.Controls.Add(this.eastTextBox);
+            this.groupBox1.Controls.Add(this.westTextBox);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Location = new System.Drawing.Point(23, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 226);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Permits Sold";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(64, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Number of Permits Sold ";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // southTextBox
+            // 
+            this.southTextBox.Location = new System.Drawing.Point(128, 155);
+            this.southTextBox.Name = "southTextBox";
+            this.southTextBox.Size = new System.Drawing.Size(100, 20);
+            this.southTextBox.TabIndex = 5;
+            this.southTextBox.AcceptsTabChanged += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // eastTextBox
+            // 
+            this.eastTextBox.Location = new System.Drawing.Point(128, 118);
+            this.eastTextBox.Name = "eastTextBox";
+            this.eastTextBox.Size = new System.Drawing.Size(100, 20);
+            this.eastTextBox.TabIndex = 4;
+            this.eastTextBox.AcceptsTabChanged += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // westTextBox
+            // 
+            this.westTextBox.Location = new System.Drawing.Point(128, 85);
+            this.westTextBox.Name = "westTextBox";
+            this.westTextBox.Size = new System.Drawing.Size(100, 20);
+            this.westTextBox.TabIndex = 3;
+            this.westTextBox.AcceptsTabChanged += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(22, 152);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 23);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "South Commuter:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(22, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 23);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "East Commuter:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(22, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 23);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "West Commuter:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.totalLabel);
+            this.groupBox2.Controls.Add(this.southLabel);
+            this.groupBox2.Controls.Add(this.eastLabel);
+            this.groupBox2.Controls.Add(this.westLabel);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(316, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 226);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Permits Revenue";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.totalLabel.Location = new System.Drawing.Point(127, 137);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalLabel.TabIndex = 7;
+            this.totalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // southLabel
+            // 
+            this.southLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.southLabel.Location = new System.Drawing.Point(127, 104);
+            this.southLabel.Name = "southLabel";
+            this.southLabel.Size = new System.Drawing.Size(100, 23);
+            this.southLabel.TabIndex = 6;
+            this.southLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // eastLabel
+            // 
+            this.eastLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eastLabel.Location = new System.Drawing.Point(127, 67);
+            this.eastLabel.Name = "eastLabel";
+            this.eastLabel.Size = new System.Drawing.Size(100, 23);
+            this.eastLabel.TabIndex = 5;
+            this.eastLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // westLabel
+            // 
+            this.westLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.westLabel.Location = new System.Drawing.Point(127, 35);
+            this.westLabel.Name = "westLabel";
+            this.westLabel.Size = new System.Drawing.Size(100, 23);
+            this.westLabel.TabIndex = 4;
+            this.westLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Total Revenue";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "South Commuter:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "East Commuter:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "West Commuter:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(103, 254);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(120, 52);
+            this.CalculateButton.TabIndex = 2;
+            this.CalculateButton.Text = "Calculate Revenue";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(255, 254);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 52);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(405, 254);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 52);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 334);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Parking Revenue";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox southTextBox;
+        private System.Windows.Forms.TextBox eastTextBox;
+        private System.Windows.Forms.TextBox westTextBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label westLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label southLabel;
+        private System.Windows.Forms.Label eastLabel;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ GarciaHW2/GarciaHW2/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarciaHW2
+    // Ismael Garcia
+    // Assignment 2
+    // Due 2-19-16
+    // Permit Revenue
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        { 
+            //This will close the program
+            this.Close();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {   
+            try {
+
+                Decimal westcom, eastcom, southcom, totalrev;
+                const int westRev = 140, eastRev = 118, southRev = 118;
+
+               
+                westcom = Decimal.Parse(westTextBox.Text);
+                eastcom = Decimal.Parse(eastTextBox.Text);
+                southcom = Decimal.Parse(southTextBox.Text);
+
+                // This will display the the total of each in the label box
+                westLabel.Text = (westcom * westRev).ToString("c0");
+                eastLabel.Text = (eastcom * eastRev).ToString("c0");
+                southLabel.Text = (southcom * southRev).ToString("c0");
+
+                // this is the calculation of total revenue
+                totalrev = (westcom * westRev) + (eastcom * eastRev) + (southcom * southRev);
+
+                // this will display the total revenue in the total label box
+                totalLabel.Text = totalrev.ToString("c0");
+            }
+            catch
+            {
+                // this will display an error if something other than a number is entered
+                MessageBox.Show("use numeric values only", "Error", MessageBoxButtons.OK , MessageBoxIcon.Error);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // this will clear the text and label boxes
+            westTextBox.Text = "";
+            eastTextBox.Text = "";
+            southTextBox.Text = "";
+            westLabel.Text = "";
+            eastLabel.Text = "";
+            southLabel.Text = "";
+            totalLabel.Text = "";
+
+
+        }
+    }
+}
