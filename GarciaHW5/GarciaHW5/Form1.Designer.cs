145f4c8d5d214ad39ebd060ef69c9b15e0df7837
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2018-11-26 12:04:47 -0700

    Add files via upload

---
+++ GarciaHW5/GarciaHW5/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GarciaHW5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.futureTextBox = new System.Windows.Forms.TextBox();
+            this.InterestTextBox = new System.Windows.Forms.TextBox();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.PresentLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Future Value:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Interest Rate:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Years:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Present Day Value:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // futureTextBox
+            // 
+            this.futureTextBox.Location = new System.Drawing.Point(118, 12);
+            this.futureTextBox.Name = "futureTextBox";
+            this.futureTextBox.Size = new System.Drawing.Size(100, 20);
+            this.futureTextBox.TabIndex = 4;
+            // 
+            // InterestTextBox
+            // 
+            this.InterestTextBox.Location = new System.Drawing.Point(118, 55);
+            this.InterestTextBox.Name = "InterestTextBox";
+            this.InterestTextBox.Size = new System.Drawing.Size(100, 20);
+            this.InterestTextBox.TabIndex = 5;
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(118, 95);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.numberTextBox.TabIndex = 6;
+            // 
+            // PresentLabel
+            // 
+            this.PresentLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PresentLabel.Location = new System.Drawing.Point(118, 179);
+            this.PresentLabel.Name = "PresentLabel";
+            this.PresentLabel.Size = new System.Drawing.Size(100, 23);
+            this.PresentLabel.TabIndex = 7;
+            this.PresentLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(15, 138);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(96, 138);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(177, 138);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.PresentLabel);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.InterestTextBox);
+            this.Controls.Add(this.futureTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Present Value Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox futureTextBox;
+        private System.Windows.Forms.TextBox InterestTextBox;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label PresentLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ GarciaHW5/GarciaHW5/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarciaHW5
+{
+    //Ismael Garcia 
+    //Assignment 5
+    //Present Value
+    //Due Date 3-18-16
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Declare variables
+        double future, interest, years, present;
+
+        private double PresentValue(double futureCalc, double interestCalc, double yearCalc)
+        { // Math Equation
+            return (futureCalc / (Math.Pow((1 + interestCalc), (yearCalc))));
+            
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //assigns the text boxes
+                
+                interest = double.Parse(InterestTextBox.Text) / 100;
+                future = double.Parse(futureTextBox.Text);
+                years = double.Parse(numberTextBox.Text);
+
+                present = PresentValue(future, interest, years);
+                PresentLabel.Text = present.ToString("c");
+
+            }
+            catch
+            { //display error message
+                MessageBox.Show("Please Enter Numeric value","Error");
+            }
+
+            
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // clear the boxes
+            futureTextBox.Clear();
+            InterestTextBox.Clear();
+            numberTextBox.Clear();
+            PresentLabel.Text = "";
+
+        }
+
+        
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // this will close the program
+            this.Close();
+        }
+
+
+
+
+        
+    }
+}
