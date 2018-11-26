d8130e4c54a98c7279d0e8e8772700bd8c18cf96
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2018-11-26 12:03:38 -0700

    Add files via upload

---
+++ GarciaHw3/GarciaHw3/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace GarciaHw3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WorkshopListBox = new System.Windows.Forms.ListBox();
+            this.LocationListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.registrationLabel = new System.Windows.Forms.Label();
+            this.lodgingLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WorkshopListBox
+            // 
+            this.WorkshopListBox.FormattingEnabled = true;
+            this.WorkshopListBox.Items.AddRange(new object[] {
+            "Handling Stress",
+            "Time Management",
+            "Supervision Skills",
+            "Negotiation",
+            "How to Interview"});
+            this.WorkshopListBox.Location = new System.Drawing.Point(12, 43);
+            this.WorkshopListBox.Name = "WorkshopListBox";
+            this.WorkshopListBox.Size = new System.Drawing.Size(120, 95);
+            this.WorkshopListBox.TabIndex = 0;
+            // 
+            // LocationListBox
+            // 
+            this.LocationListBox.FormattingEnabled = true;
+            this.LocationListBox.Items.AddRange(new object[] {
+            "Austin",
+            "Chicago",
+            "Dallas",
+            "Orlando",
+            "Phoenix",
+            "Raleigh"});
+            this.LocationListBox.Location = new System.Drawing.Point(188, 43);
+            this.LocationListBox.Name = "LocationListBox";
+            this.LocationListBox.Size = new System.Drawing.Size(120, 95);
+            this.LocationListBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Registration Cost:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(32, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lodging Cost:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(47, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total Cost:";
+            // 
+            // registrationLabel
+            // 
+            this.registrationLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.registrationLabel.Location = new System.Drawing.Point(118, 182);
+            this.registrationLabel.Name = "registrationLabel";
+            this.registrationLabel.Size = new System.Drawing.Size(100, 23);
+            this.registrationLabel.TabIndex = 5;
+            // 
+            // lodgingLabel
+            // 
+            this.lodgingLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lodgingLabel.Location = new System.Drawing.Point(118, 221);
+            this.lodgingLabel.Name = "lodgingLabel";
+            this.lodgingLabel.Size = new System.Drawing.Size(100, 23);
+            this.lodgingLabel.TabIndex = 6;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalLabel.Location = new System.Drawing.Point(118, 262);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(100, 23);
+            this.totalLabel.TabIndex = 7;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(295, 178);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(295, 217);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(295, 257);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Workshop";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(185, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Location";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(456, 294);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.lodgingLabel);
+            this.Controls.Add(this.registrationLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LocationListBox);
+            this.Controls.Add(this.WorkshopListBox);
+            this.Name = "Form1";
+            this.Text = "Fee Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox WorkshopListBox;
+        private System.Windows.Forms.ListBox LocationListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label registrationLabel;
+        private System.Windows.Forms.Label lodgingLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GarciaHw3/GarciaHw3/Form1.cs
@@ -0,0 +1,483 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GarciaHw3
+    //Ismael Garcia
+    //Homework 3
+    //Due Date 3/4/16
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            // Declare varibales, two string , inteagers
+
+            string workshop , location;
+            int days = 0, totalCost, registrationcost = 0, lodgingcost, lodgingFee = 0 ;
+
+
+
+
+            try
+            {
+
+                workshop = WorkshopListBox.SelectedItem.ToString();
+                location = LocationListBox.SelectedItem.ToString();
+
+                if (workshop == "Handling Stress" && location == "Austin")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 1000;
+
+                    // formula for to calculate
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+
+                    
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+
+                }
+                else if (workshop == "Time Management" && location == "Chicago")
+                {
+                    days = 3;
+                    lodgingFee = 225;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Handling Stress" && location == "Chicago")
+                {
+                    days = 3;
+                    lodgingFee = 225;
+                    registrationcost = 1000;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Handling Stress" && location == "Dallas")
+                {
+                    days = 3;
+                    lodgingFee = 174;
+                    registrationcost = 1000;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Handling Stress" && location == "Orlando")
+                {
+                    days = 3;
+                    lodgingFee = 300;
+                    registrationcost = 1000;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Handling Stress" && location == "Phoenix")
+                {
+                    days = 3;
+                    lodgingFee = 175;
+                    registrationcost = 1000;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Handling Stress" && location == "Raleigh")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 1000;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Time Management" && location == "Austin")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Time Management" && location == "Dallas")
+                {
+                    days = 3;
+                    lodgingFee = 174;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Time Management" && location == "Orlando")
+                {
+                    days = 3;
+                    lodgingFee = 300;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Time Management" && location == "Phoenix")
+                {
+                    days = 3;
+                    lodgingFee = 175;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Time Management" && location == "Raleigh")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 800;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Supervision Skills" && location == "Austin")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Supervision Skills" && location == "Chicago")
+                {
+                    days = 3;
+                    lodgingFee = 225;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if(workshop == "Supervision Skills" && location == "Dallas")
+                {
+                    days = 3;
+                    lodgingFee = 174;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Supervision Skills" && location == "Orlando")
+                {
+                    days = 3;
+                    lodgingFee = 300;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Supervision Skills" && location == "Phoenix")
+                {
+                    days = 3;
+                    lodgingFee = 175;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if(workshop == "Supervision Skills" && location == "Raleigh")
+                {
+                    days = 3;
+                    lodgingFee = 150;
+                    registrationcost = 1500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Austin")
+                {
+                    days = 5;
+                    lodgingFee = 150;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Chicago")
+                {
+                    days = 5;
+                    lodgingFee = 225;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Dallas")
+                {
+                    days = 5;
+                    lodgingFee = 174;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Orlando")
+                {
+                    days = 5;
+                    lodgingFee = 300;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Phoenix")
+                {
+                    days = 5;
+                    lodgingFee = 175;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "Negotiation" && location == "Raleigh")
+                {
+                    days = 5;
+                    lodgingFee = 150;
+                    registrationcost = 1300;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Austin")
+                {
+                    days = 1;
+                    lodgingFee = 150;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Chicago")
+                {
+                    days = 1;
+                    lodgingFee = 225;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Dallas")
+                {
+                    days = 1;
+                    lodgingFee = 174;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Orlando")
+                {
+                    days = 1;
+                    lodgingFee = 300;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Phoenix")
+                {
+                    days = 1;
+                    lodgingFee = 175;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+                else if (workshop == "How to Interview" && location == "Raleigh")
+                {
+                    days = 1;
+                    lodgingFee = 150;
+                    registrationcost = 500;
+
+
+                    lodgingcost = days * lodgingFee;
+                    totalCost = registrationcost + lodgingcost;
+
+                    lodgingLabel.Text = lodgingcost.ToString("c0");
+                    registrationLabel.Text = registrationcost.ToString("c0");
+                    totalLabel.Text = totalCost.ToString("c0");
+                }
+            }
+            catch
+            {  // this will show an error message if list boxes are not selected
+                MessageBox.Show("Please Select From Both List Boxes", "Error");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        { // this will clear the text boxes
+            lodgingLabel.Text = "";
+            registrationLabel.Text = "";
+            totalLabel.Text = "";
+
+        }
+    }
+}
