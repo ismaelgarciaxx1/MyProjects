15c936fe23e91294186ef148a8eee84a185da0e3
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2019-01-06 23:37:53 -0700

    Add files via upload

---
+++ FoodSwitch/FoodSwitch/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string food;
+            Console.WriteLine("Enter From Menu (Snack - Appetizer - Meal - Desert)");
+            food = Console.ReadLine().ToLower();
+
+            switch (food)
+            {
+
+
+                case "snack":
+                    Console.WriteLine("Cheetos");
+                    break;
+
+                case "appetizer":
+                    Console.WriteLine("Chips and Salsa");
+                    break;
+
+                case "meal":
+                    Console.WriteLine("Burger");
+                    break;
+
+                case "desert":
+                    Console.WriteLine("cup cake");
+                    break;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
