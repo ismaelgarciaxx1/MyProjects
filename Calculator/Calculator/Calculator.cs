5535a96942b4af2d8170d36eed12b626999cf5b3
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2019-01-06 23:40:59 -0700

    Add files via upload

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public int Sum(int numOne, int numTwo)
+        {
+            int answer=numOne + numTwo;
+
+            return answer;
+
+        }
+
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {     //page 277 and 321
+        static void Main(string[] args)
+        {
+            int numOne , numTwo;
+
+            Console.WriteLine("Input number one ");
+            numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input number two ");
+            numTwo = Convert.ToInt32(Console.ReadLine());
+
+
+            Calculator myCalc = new Calculator();
+
+            int answer = myCalc.Sum(numOne , numTwo);
+            Console.WriteLine("the sum of first number and the second number equals  " + answer);
+            Console.ReadLine();
+
+        }
+    }
+}
