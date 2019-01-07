4595629305ffafbdaae7183264a001be079ade7f
ismaelgarciaxx1 <44659640+ismaelgarciaxx1@users.noreply.github.com>
2019-01-06 23:40:07 -0700

    Add files via upload

---
+++ PasswordException/PasswordException/LessThanFive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordException
+{
+    public class LessThanFive : Exception
+    {
+        public LessThanFive() : base ("Need five Charactars")
+        {
+            Console.WriteLine("Need at least five");
+        }
+    }
+}
---
+++ PasswordException/PasswordException/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+
+            try
+            {
+                Console.WriteLine("enter new pass");
+                password = Console.ReadLine();
+
+                if (password.Length < 5)
+                    throw new LessThanFive();
+                
+            }
+
+            catch(LessThanFive ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
